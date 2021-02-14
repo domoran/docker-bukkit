72d96f013dd274d761b1ab2f152d80a6209f92c5
Mathias Mamsch <mmamsch@googlemail.com>
2021-02-14 01:40:09 +0100

    Modify run

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 set /p MC_VERSION=Which version to run? (e.g. 1.8.8)?
 set /p MC_DIR=Data Directory? (e.g. C:\data)?
-set /p MC_PORT=Port? (e.g. 25566)
+set /p MC_PORT=Port? (e.g. 25565)
 
-docker run -it -v %MC_DIR%:/data -p %MC_PORT%:25566  -e EULA=true --name mc_server-%MC_VERSION% mcserver-%MC_VERSION%
+docker run -it -v %MC_DIR%:/data -p %MC_PORT%:25565  -e EULA=true --name mc_server-%MC_VERSION% mcserver-%MC_VERSION%
